1f3b5ef7626902271a3e983ba1cf23d13129b2f0
Julien Bouze <julien.bouze@gmail.com>
2024-04-28 16:06:09 +0200

    Implementation of getComputerChoice function
    Creation of a function that returns rock, paper or scissors

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+function getComputerChoice(){
+    let randomNumber = Math.floor((Math.random())*3)+1;
+    switch(randomNumber){
+        case 1 :
+            return "rock";
+        case 2 :
+            return "paper";
+        case 3 :
+            return "scissors";
+        default :
+            break;
+    }
+}
+
+console.log(getComputerChoice());
