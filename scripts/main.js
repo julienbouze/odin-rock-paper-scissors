2d7197cf395947d9a4b25a01dabd9c42738a0562
Julien Bouze <julien.bouze@gmail.com>
2024-04-28 16:31:15 +0200

    Implementation of playRound function
    Creation of a function that declares the winner of a round based on choices made

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,48 @@
-function getComputerChoice(){
-    let randomNumber = Math.floor((Math.random())*3)+1;
-    switch(randomNumber){
-        case 1 :
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let randomNumber = Math.floor((Math.random()) * 3) + 1;
+    switch (randomNumber) {
+        case 1:
             return "rock";
-        case 2 :
+        case 2:
             return "paper";
-        case 3 :
+        case 3:
             return "scissors";
-        default :
+        default:
             break;
     }
 }
 
-console.log(getComputerChoice());
-
-function getHumanChoice(){
-    let humanChoice ; 
-    while (humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors"){
+function getHumanChoice() {
+    let humanChoice;
+    while (humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors") {
         humanChoice = prompt("Select rock, paper or scissors");
     }
     return humanChoice.toLowerCase();
 }
-console.log(getHumanChoice());
+
+function playRound(humanChoice,computerChoice) {
+    console.log(humanChoice);
+    console.log(computerChoice);
+    if (computerChoice == humanChoice) {
+        return "It's a draw !";
+    }
+    else if ((computerChoice == "rock" && humanChoice == "scissors")
+        || (computerChoice == "paper" && humanChoice == "rock")
+        || (computerChoice == "scissors" && humanChoice == "paper")) {
+            ++computerScore;
+            return "You lost ! " + computerChoice + " beats " + humanChoice;
+    }
+    else{
+        ++humanScore;
+        return "You won ! " + humanChoice + " beats " + computerChoice;
+    }
+}
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
